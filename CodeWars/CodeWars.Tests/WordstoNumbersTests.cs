5d565c81b892f24b4fe49b00f3c9f41d713b78f5
Neil Hargreaves <neil.hargreaves@gtlsolutions.co.uk>
2022-03-25 16:52:05 +0000

    Words to numbers functionality

---
+++ CodeWars/CodeWars.Tests/WordstoNumbersTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class WordstoNumbersTests
+    {
+        [TestMethod()]
+        public void ParseIntTest()
+        {
+            Assert.AreEqual(1, WordstoNumbers.ParseInt("one"));
+            Assert.AreEqual(20, WordstoNumbers.ParseInt("twenty"));
+            Assert.AreEqual(246, WordstoNumbers.ParseInt("two hundred forty-six"));
+            Assert.AreEqual(-146, WordstoNumbers.ParseInt("minus one hundred forty-six"));
+            Assert.AreEqual(2000166, WordstoNumbers.ParseInt("two million one hundred sixty-six"));
+            Assert.AreEqual(2001256, WordstoNumbers.ParseInt("two million one thousand two hundred and fifty-six"));
+            Assert.AreEqual(2001246, WordstoNumbers.ParseInt("two million one thousand two hundred & forty-six"));
+            Assert.AreEqual(2000000000, WordstoNumbers.ParseInt("two billion"));
+            Assert.AreEqual(8000000000000, WordstoNumbers.ParseInt("eight trillion"));
+        }
+    }
+}
---
+++ CodeWars/CodeWars/WordstoNumbers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class WordstoNumbers
+    {
+        private static readonly Dictionary<string, long> numberDictionary = new Dictionary<string, long>{
+            {"zero", 0},
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+            {"five", 5},
+            {"six", 6},
+            {"seven", 7},
+            {"eight", 8},
+            {"nine", 9},
+            {"ten", 10},
+            {"eleven", 11},
+            {"twelve", 12},
+            {"thirteen", 13},
+            {"fourteen", 14},
+            {"fifteen", 15},
+            {"sixteen", 16},
+            {"seventeen", 17},
+            {"eighteen", 18},
+            {"nineteen", 19},
+            {"twenty", 20},
+            {"thirty", 30},
+            {"forty", 40},
+            {"fifty", 50},
+            {"sixty", 60},
+            {"seventy", 70},
+            {"eighty", 80},
+            {"ninety", 90},
+            {"hundred", 100},
+            {"thousand", 1000},
+            {"million", 1000000},
+            {"billion", 1000000000},
+            {"trillion", 1000000000000}
+        };
+
+        public static long ParseInt(string s)
+        {
+            long accumulator = 0, total = 0;
+
+            var numbers = Regex.Matches(s, @"\w+")
+                .Cast<Match>()
+                .Select(m => m.Value.ToLowerInvariant())
+                .Where(v => numberDictionary.ContainsKey(v))
+                .Select(v => numberDictionary[v]);
+
+            foreach (var number in numbers)
+            {
+                if (number >= 1000)
+                {
+                    total += accumulator * number;
+                    accumulator = 0;
+                }
+                else if (number >= 100)
+                {
+                    accumulator *= number;
+                }
+                else accumulator += number;
+            }
+
+            return (total + accumulator) * (s.StartsWith("minus", StringComparison.InvariantCultureIgnoreCase) ? -1 : 1);
+        }
+    }
+}
