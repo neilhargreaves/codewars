12f6db120b3fc6924949440708e39109c034545f
Neil Hargreaves <neil.hargreaves@gtlsolutions.co.uk>
2022-03-25 14:46:22 +0000

    added rgb to hex converter

---
+++ CodeWars/CodeWars.Tests/RGBtoHEXTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class RGBtoHEXTests
+    {
+        [TestMethod()]
+        public void RgbtoHexTest()
+        {
+            Assert.AreEqual("FFFFFF", RGBtoHEX.RgbtoHex(255, 255, 255));
+            Assert.AreEqual("FFFFFF", RGBtoHEX.RgbtoHex(255, 255, 300));
+            Assert.AreEqual("000000", RGBtoHEX.RgbtoHex(0, 0, 0));
+            Assert.AreEqual("9400D3", RGBtoHEX.RgbtoHex(148, 0, 211));
+            Assert.AreEqual("9400D3", RGBtoHEX.RgbtoHex(148, -20, 211), "Handle negative numbers.");
+            Assert.AreEqual("90C3D4", RGBtoHEX.RgbtoHex(144, 195, 212));
+            Assert.AreEqual("D4350C", RGBtoHEX.RgbtoHex(212, 53, 12), "Consider single hex digit numbers.");
+        }
+    }
+}
---
+++ CodeWars/CodeWars/RGBtoHEX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public class RGBtoHEX
+    {
+        public static string RgbtoHex(int r, int g, int b)
+        {
+            r = Math.Max(Math.Min(255, r), 0);
+            g = Math.Max(Math.Min(255, g), 0);
+            b = Math.Max(Math.Min(255, b), 0);  
+
+            return $"{r:X2}{g:X2}{b:X2}";
+        }
+    }
+}
